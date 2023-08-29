198abbe34c1e877013b316146c6760b20162cc76
CarrionAndOn <maya@carrionandon.me>
2023-08-29 14:31:59 -0700

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using SLZ.Bonelab;
+using HarmonyLib;
+
+namespace FantasyColiseum
+{
+    public class Main : MelonMod
+    {
+        internal const string Name = "Fantasy Coliseum"; // Required
+        internal const string Description = "Unlocks the max enemy count in Fantasy Arena."; // Required
+        internal const string Author = "CarrionAndOn"; // Required
+        internal const string Company = "Weather Electric"; // Set as null if blank
+        internal const string Version = "1.0.0"; // Required
+        internal const string DownloadLink = "https://bonelab.thunderstore.io/package/CarrionAndOn/FantasyColiseum/"; // Set as null if blank
+
+        public override void OnInitializeMelon()
+        {
+            
+        }
+        [HarmonyPatch(typeof(ArenaMenuController), "Start")]
+        public static class ArenaPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ArenaMenuController __instance)
+            {
+                MelonLogger.Msg($"{__instance} Active");
+                __instance.maxEnemyCount = 500;
+                MelonLogger.Msg($"{__instance} max enemy count is {__instance.maxEnemyCount}");
+                __instance.maxConcurrent = 500;
+                MelonLogger.Msg($"{__instance} max concurrent is {__instance.maxConcurrent}");
+                __instance.maxFriendlyCount = 500;
+                MelonLogger.Msg($"{__instance} max friendly count is {__instance.maxFriendlyCount}");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(FantasyColiseum.Main.Description)]
+[assembly: AssemblyDescription(FantasyColiseum.Main.Description)]
+[assembly: AssemblyCompany(FantasyColiseum.Main.Company)]
+[assembly: AssemblyProduct(FantasyColiseum.Main.Name)]
+[assembly: AssemblyCopyright("Developed by " + FantasyColiseum.Main.Author)]
+[assembly: AssemblyTrademark(FantasyColiseum.Main.Company)]
+[assembly: AssemblyVersion(FantasyColiseum.Main.Version)]
+[assembly: AssemblyFileVersion(FantasyColiseum.Main.Version)]
+[assembly:
+    MelonInfo(typeof(FantasyColiseum.Main), FantasyColiseum.Main.Name, FantasyColiseum.Main.Version,
+        FantasyColiseum.Main.Author, FantasyColiseum.Main.DownloadLink)]
+[assembly: MelonColor(System.ConsoleColor.White)]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
